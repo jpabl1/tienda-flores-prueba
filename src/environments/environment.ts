b0d544035acff67349faa2b021a3d6169ded1fd9
Juan Pablo Castillo Montellano <76228621+jpabl1@users.noreply.github.com>
2025-05-16 17:42:51 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     appId: '1:95165450684:web:238f4204daac7eda3a83b9',
     storageBucket: 'app-flores-001.appspot.com',
     locationId: 'us-central',
-    apiKey: 'AIzaSyBBHnB5LQNlL3kHQfHFnPo2mL9jMa2oeao',
+    apiKey: '',
     authDomain: 'app-flores-001.firebaseapp.com',
     messagingSenderId: '95165450684',
     measurementId: 'G-MJ6TZGEC15',
